09780958bbfa1bbf91aa8cf3b136a84633fd5c5e
crowleey33 <107879077+crowleey33@users.noreply.github.com>
2022-06-20 19:13:32 +0200

    Add files via upload

---
+++ feladat01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Feladat01
+{
+    class Feladat01
+    {
+        static void Main(string[] args)
+        {
+            string[] szinek = { "piros", "kék", "zöld", "sárga", "fekete" };  // INPUT                  
+
+            int output = feladat01(szinek);   //OUTPUT
+        }
+
+        static int feladat01(string[] szinek)
+        {
+            int db = szinek.Length;
+            int k = db;
+            int eredmeny = 0;
+
+            if (db <= 1)
+            {
+                return 0;
+            }
+            else
+            {
+                while (k > 1)
+                {
+                    eredmeny += Faktorialis(db) / (Faktorialis(k) * Faktorialis(db - k));
+                    k--;
+                }
+            }
+            return eredmeny;
+        }
+
+        static int Faktorialis(int faktorialis)
+        {
+            int eredmeny = 1;
+
+            while (faktorialis >= 1)
+            {
+                eredmeny *= faktorialis;
+                faktorialis--;
+            }
+            return eredmeny;
+        }
+    }
+}
